c3055b5dc1a81b69f43ec28c33c8e46827c81661
Rebeca Prado da Silva <rebecaprado@alunos.utfpr.edu.br>
2026-05-03 14:12:21 -0300

    terminando bhaskara

---
+++ PrimeiroBimestre_2026/Bhaskara.js
@@ -0,0 +1,31 @@
+function calcular(){
+    let a = parseFloat(document.getElementById("inputA").value);
+    let b = parseFloat(document.getElementById("inputB").value);
+    let c = parseFloat(document.getElementById("inputC").value);
+
+    let footer = document.getElementById("footer");
+    let mensagem = document.getElementById("mensagem");
+
+    if(isNaN(a&&b&&c)){
+        footer.classList.add("erro");
+        mensagem.innerHTML = "Os espaços precisam ser preenchidos";
+        X1 = "-";
+        X2 = "-";
+    }else{
+        footer.classList.remove("erro");
+        mensagem.innerHTML = "Claculo realizado com sucesso";
+    }
+
+    let d = b**2-4*a*c;
+
+    let X1 = (-b+(d**(1/2)))/(2*a);
+    let X2 = (-b-(d**(1/2)))/(2*a);
+
+    if(isNaN(X1) || isNaN(X2)){
+        X1 = "Não é possível calcular a raiz de delta. ∆ = " + d;
+        X2 = "Não é possível calcular a raiz de delta. ∆ = " + d;
+    }
+
+    document.getElementById("x1").innerHTML = X1;
+    document.getElementById("x2").innerHTML = X2;
+}
