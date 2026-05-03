a996044149f31909d07ecac6ad43b1600d650aac
Rebeca Prado da Silva <rebecaprado@alunos.utfpr.edu.br>
2026-05-03 17:03:42 -0300

    mudanças em Bhaskara e IMC

--- PrimeiroBimestre_2026/IMC/IMC.js
+++ PrimeiroBimestre_2026/IMC/IMC.js
@@ -1,43 +1,43 @@
-function funcaoCalcularIMC() {
+function CalcularIMC(){
     let p = parseFloat(document.getElementById("inputPeso").value);
     let a = parseFloat(document.getElementById("inputAltura").value);
 
-    let imc = p / (a * a);
-    let i = imc.toFixed(2);
-
     let footer = document.getElementById("footer");
     let mensagem = document.getElementById("mensagem");
-    let classific = "";
 
-    if(isNaN(p) || isNaN(a)){
+    if(isNaN(p)|| isNaN(a)){
         footer.classList.add("erro");
-        mensagem.innerHTML = "Os espaços precisam ser preenchidos";
+        mensagem.innerHTML = "Os espaços precisam ser preenchidos com números";
+        imc = "-";
         classific = "-";
-        i = "-";
-    } else if(p <= 0 || a <= 0){
+    }else if(p <= 0 || a <= 0){
         footer.classList.add("erro");
-        mensagem.innerHTML = "Os valores não podem ser negativos ou iguais a 0";
+        mensagem.innerHTML = "Os valores precisam ser maiores que 0";
+        imc = "-";
         classific = "-";
-        i = "-";
     }else{
         footer.classList.remove("erro");
-        mensagem.innerHTML = "Cálculo realizado com sucesso";
-        
-        if(imc < 18.5){
-            classific = "Abaixo do peso";
-         }else if(imc < 25){
-            classific = "Peso normal";
-        }else if(imc < 30){
-            classific = "Sobrepeso";
-        }else if(imc < 35){
-            classific = "Obesidade grau I";
-        }else if(imc < 40){
-            classific = "Obesidade grau II";
-        }else if(imc >= 40){
-            classific = "Obesidade grau III";
-        }
+        mensagem.innerHTML = "Calculo realizado com sucesso";
+    }
+
+    let imc = p/(a*a);
+    let i = imc.toFixed(2);
+    let classific = "";
+
+    if(imc<18.5){
+        classific = "Abaixo do peso";
+    }else if(imc<=24.9){
+        classific = "Peso normal";
+    }else if(imc<=29.9){
+        classific = "Sobrepeso";
+    }else if(imc<=34.9){
+        classific = "Obesidade grau I";
+    }else if(imc<=39.9){
+        classific = "Obesidade grau II";
+    }else if(imc>=40){
+        classific = "Obesidade grau III";
     }
 
     document.getElementById("respIMC").innerHTML = i;
-    document.getElementById("respClassificacao").innerHTML = classific;
+    document.getElementById("respClassific").innerHTML = classific;
 }
